1e54f945d2a066d987f3172395983704799a3b52
Pia <algauer.henrique@gmail.com>
2025-01-06 17:54:15 -0300

    fim do expediente

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
+// import { authMiddleware } from "@clerk/nextjs";
+
+// export default authMiddleware()
+
+// export const config = {
+//     // matcher: ["/((?!.*\\..*|_next).*", "/", "/(api|trpc)(.*)"]
+//     matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// }
+
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware()
+export default authMiddleware({
+    publicRoutes: ["/api/:path*"]
+});
 
 export const config = {
-    // matcher: ["/((?!.*\\..*|_next).*", "/", "/(api|trpc)(.*)"]
     matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+};  
