54a967970e7d8576af3dbfa067aa62a58c802180
henriqueAlgauer <algauer.henrique@gmail.com>
2025-01-06 22:41:57 -0300

    adicionado modal na /

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,25 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import { Modal } from "@/components/ui/modal"
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { useEffect } from "react"
+
+const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen)
+    const isOpen = useStoreModal((state) => state.isOpen)
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen()
+        }
+    }, [isOpen, onOpen])
+
     return (
         <div>
-            <Button size='sm'>Click Me</Button>
+            Root Page
         </div>
     )
 }
+
+
+export default SetupPage
