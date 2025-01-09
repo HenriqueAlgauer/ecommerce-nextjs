3ad494d281e0423c1b2753fe47d1147ae4ee9609
henriqueAlgauer <algauer.henrique@gmail.com>
2025-01-09 00:37:03 -0300

    modal excluir funcionando

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,15 +1,27 @@
 'use client'
-import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
-import { Heading } from "@/components/ui/heading"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
-import { zodResolver } from "@hookform/resolvers/zod"
+
+import * as z from "zod"
+import axios from "axios"
+import { useState } from "react"
 import { Store } from "@prisma/client"
 import { Trash } from "lucide-react"
-import { useState } from "react"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { toast } from "react-hot-toast"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useParams, useRouter } from "next/navigation"
+
+import { AlertModal } from "@/components/modals/alert-modal"
+import { Heading } from "@/components/ui/heading"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 interface SettingsFormProps {
     initialData: Store
@@ -24,6 +36,9 @@ type SettingsFormValues = z.infer<typeof formSchema>
 export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
+    const params = useParams()
+    const router = useRouter()
+
     const [open, setOpen] = useState(false)
     const [loading, setLoading] = useState(false)
 
@@ -33,11 +48,26 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     })
 
     const onSubmit = async (data: SettingsFormValues) => {
-        console.log(data)
+        try {
+            setLoading(true)
+            await axios.patch(`/api/stores/${params.storeId}`, data)
+            router.refresh()
+            toast.success('Store updated')
+        } catch (error) {
+            toast.error('Something went wrong')
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
         <>
+            <AlertModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={() => { }}
+                loading={loading}
+            />
             <div className='flex items-center justify-between'>
                 <Heading
                     title='Settings'
