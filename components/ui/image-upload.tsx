5c764d8facc4cf8abd2c5d1fc29761e41cfcfc87
Pia <algauer.henrique@gmail.com>
2025-01-13 12:19:59 -0300

    adicionado presets cloudinary

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,6 +1,11 @@
 'use client'
 
+import Image from "next/image"
 import { useEffect, useState } from "react"
+import { Trash } from "lucide-react"
+import { CldUploadWidget } from 'next-cloudinary'
+
+import { Button } from "./button"
 
 interface ImageUploadProps {
     disabled?: boolean
@@ -21,12 +26,36 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
         setIsMouted(true)
     }, [])
 
+    const onUpload = (result: any) => {
+        onChange(result.info.secure_url)
+    }
+
     if (!isMounted) {
         return null
     }
+
     return (
         <div>
-            ImageUpload
+            <div className='mb-4 flex items-center gap-4'>
+                {value.map((url) => (
+                    <div key={url} className='relative w-[200px] h-[200px] rounded-md overflow-'>
+                        <div className='z-10 absolute top-2 right-2'>
+                            <Button type='button' onClick={() => onRemove(url)} variant='destructive' size='icon'>
+                                <Trash className='h-4 w-4' />
+                            </Button>
+                        </div>
+                        <Image
+                            fill
+                            className='object-cover'
+                            alt='Image'
+                            src={url}
+                        />
+                    </div>
+                ))}
+            </div>
+            <CldUploadWidget onUpload={onUpload} uploadPreset="">
+
+            </CldUploadWidget>
         </div>
     )
 }
