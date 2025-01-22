9df5a79a53b6f1dc60c411678c7ca8742aee5bab
henriqueAlgauer <algauer.henrique@gmail.com>
2025-01-21 22:24:13 -0300

    adicionado tabela com dados das billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,35 +1,37 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { Heading } from "@/components/ui/heading"
-import { Separator } from "@/components/ui/separator"
-import { Billboard } from "@prisma/client"
-import { Plus } from "lucide-react"
-import { useParams, useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./colums";
+import { DataTable } from "@/components/ui/data-table";
 
 interface BillboardClientProps {
-    data: Billboard[]
+  data: BillboardColumn[];
 }
 
-export const BillBoardClient: React.FC<BillboardClientProps> = ({
-    data
-}) => {
-    const router = useRouter()
-    const params = useParams()
+export const BillBoardClient: React.FC<BillboardClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
 
-    return (
-        <>
-            <div className='flex items-center justify-between'>
-                <Heading
-                    title={`Billboards (${data.length})`}
-                    description="Manage billboards for your store"
-                />
-                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
-                    <Plus className='mr-2 h-4 w-4' />
-                    Add New
-                </Button>
-            </div>
-            <Separator />
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="label" columns={columns} data={data} />
+    </>
+  );
+};
